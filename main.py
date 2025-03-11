101fd0cc40e298d9a5c6bb8d6c4fdbe5d57414e0
Krystian Turek <kry.turek@gmail.com>
2025-03-11 14:23:56 +0000

    difficulty adjusted

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sudoku_generator import generate_puzzle
 
 # Generate and print a Sudoku puzzle with a specified number of holes
-num_holes = 46  # Adjust the number of holes based on desired difficulty
+num_holes = 50  # Adjust the number of holes based on desired difficulty
 sudoku_puzzle = generate_puzzle(num_holes)
 for row in sudoku_puzzle:
     print(row)
